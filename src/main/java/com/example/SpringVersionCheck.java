56795cba8d81173961c3ec6b7d993a25bc3a623c
Raj Mevawala <rajmevawala23@icloud.com>
2026-01-29 14:54:31 +0530

    [INIT] Add Spring dependencies and project setup

---
+++ src/main/java/com/example/SpringVersionCheck.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import org.springframework.core.SpringVersion;
+
+public class SpringVersionCheck {
+    public static void main(String[] args) {
+        System.out.println(SpringVersion.getVersion());
+    }
+}
