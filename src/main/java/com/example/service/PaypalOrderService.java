c51d2466631f135dbb99f68e94cadf5a5202efb7
Raj Mevawala <rajmevawala23@icloud.com>
2026-02-10 23:26:02 +0530

    [IMP] PaypalOrderService

---
+++ src/main/java/com/example/service/PaypalOrderService.java
@@ -0,0 +1,17 @@
+package com.example.service;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaypalOrderService {
+    private final PaymentService paymentService;
+
+    public  PaypalOrderService(@Qualifier("PaypalPaymentService") PaymentService paymentService){
+        this.paymentService=paymentService;
+    }
+    public  void placeOrder(double amount){
+        paymentService.makePayment(amount);
+        System.out.println("PayPal order placed successfully.");
+    }
+}
